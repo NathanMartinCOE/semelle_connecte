14c3ddf77b43a859227b4fe1155baa9d430f3ec5
NathanMartinCOE <124940887+NathanMartinCOE@users.noreply.github.com>
2023-04-12 14:19:54 +0200

    Add Tools Interpolation
    This function normalized any Ground Reaction force step in % of cycle by
        interpolated
    
        Agrs: Ground Reaction Force of one step
    
        Output:
            new x value : for plot
            new y value : Ground Reaction force normalised in % of cycle

---
+++ Tools/ToolsInterpolationGrf.py
@@ -0,0 +1,25 @@
+# coding utf-8
+# Author : Nathan Martin 
+# Create : 2023 - 04 - 07
+
+""" This function normalized any Ground Reaction force step in % of cycle by
+    interpolated
+
+    Agrs: Ground Reaction Force of one step
+
+    Output: 
+        new x value : for plot
+        new y value : Ground Reaction force normalised in % of cycle
+"""
+
+import numpy as np
+from scipy import interpolate
+
+
+def InterpolationGrf(Grf):
+    x = np.linspace(0,len(Grf),len(Grf))
+    y = Grf
+    f = interpolate.interp1d(x, y)
+    xnew = np.linspace(0, len(Grf), 100)
+    ynew = f(xnew)
+    return xnew, ynew
