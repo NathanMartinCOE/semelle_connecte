94a339160c1db3d143dd44542323aa12c5984739
NathanMartinCOE <124940887+NathanMartinCOE@users.noreply.github.com>
2023-05-18 14:00:33 +0200

    Update MottekGait.py

--- MottekGait.py
+++ MottekGait.py
@@ -89,18 +89,18 @@ for path, name in zip(list_path, list_name):
     X.append(x)
     Labels.append(labels)
 
-combine_x = [X[0][0], X[1][0], X[2][0], X[0][1], X[1][1], X[2][1], X[0][2],  X[1][2], X[2][2]]
-combine_labels = [Labels[0][0], Labels[1][0], Labels[2][0], Labels[0][1], Labels[1][1], Labels[2][1], Labels[0][2], Labels[1][2], Labels[2][2]]
+# combine_x = [X[0][0], X[1][0], X[2][0], X[0][1], X[1][1], X[2][1], X[0][2],  X[1][2], X[2][2]]
+# combine_labels = [Labels[0][0], Labels[1][0], Labels[2][0], Labels[0][1], Labels[1][1], Labels[2][1], Labels[0][2], Labels[1][2], Labels[2][2]]
 
-plt.figure()
-plt.boxplot(x= combine_x, labels= combine_labels)
-plt.hlines(y=5, xmin=0, xmax= (len(combine_x)+1) , ls="--", colors="black")
-plt.hlines(y=-5, xmin=0, xmax= (len(combine_x)+1), ls="--", colors="black")
-low, high = plt.ylim()
-bound = max(abs(low), abs(high))
-plt.ylim(-bound, bound)
-plt.ylabel("Values of Dynamic Assymetry Function")
-plt.show()
+# plt.figure()
+# plt.boxplot(x= combine_x, labels= combine_labels)
+# plt.hlines(y=5, xmin=0, xmax= (len(combine_x)+1) , ls="--", colors="black")
+# plt.hlines(y=-5, xmin=0, xmax= (len(combine_x)+1), ls="--", colors="black")
+# low, high = plt.ylim()
+# bound = max(abs(low), abs(high))
+# plt.ylim(-bound, bound)
+# plt.ylabel("Values of Dynamic Assymetry Function")
+# plt.show()
 
 
 # """ Début des procédure 2 (cf nom de la procédure et documentation) """
@@ -126,6 +126,19 @@ plt.show()
     # procedure = PlotWorthAndBestStepProcedure()
     # WalkingGraphicsFilter(walking, procedure).run()
 
+
+    from Walking.WalkingFilters import WalkingDataProcessingFilter
+    from Walking.WalkingDataProcessingProcedure import CutDataProcessingProcedure
+
+    procedure = CutDataProcessingProcedure()
+    procedure.setCutNumber(n_cut=3)
+    WalkingDataProcessingFilter(walking, procedure).run()
+
+    from Walking.WalkingFilters import WalkingGraphicsFilter
+    from Walking.WalkingGraphicsProcedure import PlotCutGroundReactionForceProcedure
+    
+    procedure = PlotCutGroundReactionForceProcedure()
+    WalkingGraphicsFilter(walking, procedure).run()
 
 
 
