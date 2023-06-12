253c804ab67f2c1d119c581718ff293e337566a0
NathanMartinCOE <124940887+NathanMartinCOE@users.noreply.github.com>
2023-06-12 21:45:47 +0200

    Create DFA_test.py

---
+++ work_in_progress/DFA_test.py
@@ -0,0 +1,33 @@
+# test Detrended Fluctuation Analysis
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import fathon
+from fathon import fathonUtils as fu
+
+#time series
+a = np.random.randn(10000)
+
+#zero-mean cumulative sum
+a = fu.toAggregated(a)
+
+#initialize dfa object
+pydfa = fathon.DFA(a)
+
+#compute fluctuation function and Hurst exponent
+wins = fu.linRangeByStep(10, 2000)
+n, F = pydfa.computeFlucVec(wins, revSeg=True, polOrd=3)
+H, H_intercept = pydfa.fitFlucVec()
+
+import ipdb; ipdb.set_trace()
+
+#compute Hurst exponent in different ranges
+limits_list = np.array([[15,2000], [200,1000]], dtype=int)
+list_H, list_H_intercept = pydfa.multiFitFlucVec(limits_list)
+
+
+import ipdb; ipdb.set_trace()
+plt.figure()
+plt.plot(a)
+plt.show()
